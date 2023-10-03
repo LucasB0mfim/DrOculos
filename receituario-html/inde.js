08ce3a0412eb19cd7148ba934bcfaf8ac599a6a8
Lucas Bomfim <lucasbomfimsobral@gmail.com>
2023-10-03 11:17:51 -0300

    atualização

--- receituario-html/inde.js
+++ receituario-html/inde.js
@@ -2,4 +2,17 @@ $(document).ready(function() {
     $(".menu").click(function() {
         $("nav").slideToggle();
     })
+    var valorInput = $("#valor");
+
+    $("#aumentar").click(function() {
+        var valorAtual = parseInt(valorInput.val());
+        valorInput.val(valorAtual + 1);
+    });
+
+    $("#diminuir").click(function() {
+        var valorAtual = parseInt(valorInput.val());
+        if (valorAtual > 0) {
+            valorInput.val(valorAtual - 1);
+        }
+    });
 }) 
